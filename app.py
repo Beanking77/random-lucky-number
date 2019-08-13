78aa139f9f18688cbccb6cb664d4c941fb3fd1cd
Bean <beanking77@gmail.com>
2019-08-13 18:14:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -45,12 +45,12 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    var msg = ""
-    url="http://www.9800.com.tw/lotto38/statistics.html"
+    msg = ""
+    url = "http://www.9800.com.tw/lotto38/statistics.html"
     msg += "20: %s\n" % str(getMagicNumber(url)) 
-    url="http://www.9800.com.tw/lotto38/statistics10.html"
+    url = "http://www.9800.com.tw/lotto38/statistics10.html"
     msg += "10: %s\n" % str(getMagicNumber(url))
-    url="http://www.9800.com.tw/lotto38/statistics50.html"
+    url = "http://www.9800.com.tw/lotto38/statistics50.html"
     msg += "50: %s\n" % str(getMagicNumber(url))
     line_bot_api.reply_message(
         event.reply_token,
