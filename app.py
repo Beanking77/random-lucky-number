42fc9beba145695a0ad2e8fa389fe03d00d313f7
Bean <beanking77@gmail.com>
2019-08-14 12:01:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="目前支援關鍵字: wl, bl"))   
-        break
+        return
     elif (game_type == "wl"):
         game_category = "lotto38"
         game_name = "威力彩"
